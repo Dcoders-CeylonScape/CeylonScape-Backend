c03f480b677b5bdfd79cdef49b7e7aae455f2e5b
Malindu De Silva <malindupasan@gmail.com>
2024-09-05 03:05:53 +0530

    added database

---
+++ AuthAPI/Models/Passport.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AuthAPI.Models;
+public class Passport
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int UserInfoId { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    public string Number { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string PlaceOfIssue { get; set; }
+
+    [Required]
+    public DateTime DateOfIssue { get; set; }
+
+    [Required]
+    public DateTime DateOfExpiry { get; set; }
+
+    public bool IsPrevious { get; set; }
+
+    public UserInfo UserInfo { get; set; }
+}
---
+++ AuthAPI/Models/UserInfo.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AuthAPI.Models;
+
+public class UserInfo
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string FullName { get; set; }
+
+    [MaxLength(50)]
+    public string Nationality { get; set; }
+
+    [MaxLength(10)]
+    public string Gender { get; set; }
+
+    public DateTime DOB { get; set; }
+    public string BirthCountry { get; set; }
+    public string BirthPlace { get; set; }
+
+    public double Height { get; set; }
+
+    [MaxLength(255)]
+    public string Peculiarity { get; set; }
+
+    [MaxLength(255)]
+    public string DomicileAddress { get; set; }
+
+    [MaxLength(255)]
+    public string AddressDuringSriLanka { get; set; }
+
+    [MaxLength(20)]
+    public string Telephone { get; set; }
+
+    [MaxLength(20)]
+    public string Mobile { get; set; }
+
+    [MaxLength(20)]
+    public string Fax { get; set; }
+
+    [MaxLength(100)]
+    public string Email { get; set; }
+
+    public CivilStatus CivilStatus { get; set; }
+
+    public ICollection<EmergencyContact> EmergencyContacts { get; set; }
+
+    public NaturalizationInfo NaturalizationInfo { get; set; }
+
+    public SpouseInfo Spouse { get; set; }
+
+    public Passport Passport { get; set; }
+
+    public ICollection<EntryVisaInfo> EntryVisas { get; set; }
+
+    public ICollection<Children> Children { get; set; }
+
+    public Profession Profession { get; set; }
+
+    public ResidenceVisaInfo ResidenceVisaInfo { get; set; }
+    
+    [ForeignKey("User")]
+    public int UserID { get; set; }
+ 
+    // user extention
+}
+
+public enum CivilStatus
+{
+    Single,
+    Married,
+    Widowed,
+    Divorced
+}
