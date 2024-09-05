63bedc460ef42544490e95c4d4fa8b74ff88b5c2
Malindu De Silva <malindupasan@gmail.com>
2024-09-05 15:36:03 +0530

    updated db

--- AuthAPI/obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
+++ AuthAPI/obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89399feaaf995adf47240cdc9decc2f4cb4e380a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbc2947e29fbea55470dc03b452d15a2ae6793d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
